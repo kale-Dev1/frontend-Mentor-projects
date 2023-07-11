59a404bb073c880f512fd6648a7587f066b68a6d
kale-Dev1 <kamarakale4@gmail.com>
2023-07-10 23:55:30 -0500

    Add error messages and form validation

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -1,23 +1,72 @@
 const button = document.querySelector(".enter-btn");
-const userYear = document.querySelector(".entered-year");
-const userMonth = document.querySelector(".entered-month");
-const userDay = document.querySelector(".entered-day");
+const form = document.getElementById("birthday-form");
+const yearError = document.querySelector(".year-error");
+const monthError = document.querySelector(".month-error");
+const dayError = document.querySelector(".day-error");
+
+const labels = document.querySelectorAll("label");
+const inputs = document.querySelectorAll("input");
 
 const years = document.querySelector(".years");
 const months = document.querySelector(".months");
 const days = document.querySelector(".days");
 
+function runError() {
+  [...labels].forEach((label) => (label.style.color = "red"));
+  [...inputs].forEach((input) => (input.style.borderColor = "red"));
+}
+function removeError() {
+  [...labels].forEach((label) => (label.style.color = "black"));
+  [...inputs].forEach(
+    (input) => (input.style.borderColor = "hsl(259, 100%, 65%)")
+  );
+}
+
 button.addEventListener("click", function (e) {
-  e.preventDefault();
   const birthDay = document.getElementById("day").value;
   const birthMonth = document.getElementById("month").value;
   const birthYear = document.getElementById("year").value;
+
+  e.preventDefault();
+
+  function checkYear(birthYear) {
+    if (birthYear === "" || birthYear > 2023 || birthYear < 1000) {
+      yearError.textContent = "Enter a valid year";
+      return false;
+    }
+    removeError();
+    yearError.textContent = "";
+    return true;
+  }
+
+  function checkMonth(birthMonth) {
+    if (birthMonth === "" || birthMonth > 12) {
+      monthError.textContent = "Enter a valid Month";
+      return false;
+    }
+    removeError();
+    monthError.textContent = "";
+    return true;
+  }
+  if (birthDay === "" || birthDay > 31) {
+    dayError.textContent = "Enter a valid day";
+    runError();
+  }
+
+  // fUNCTION TO ADD yEARS TO THE PAGE
   const birthdate = [birthYear, birthMonth, birthDay];
   const age = calculateAge(birthdate.join(","));
+  function addYears() {
+    years.textContent = age.years;
+    months.textContent = age.months;
+    days.textContent = age.days;
+  }
 
-  years.textContent = age.years;
-  months.textContent = age.months;
-  days.textContent = age.days;
+  if (checkMonth(birthMonth) && checkYear(birthYear)) {
+    addYears();
+  } else {
+    runError();
+  }
 });
 
 // Function to calculate age and return years, months and days as object
@@ -53,7 +102,10 @@ function calculateAge(birthDate) {
     // )(1000 * 60 * 60 * 24);
     days = Math.floor(days);
     days += daysInLastMonth;
+    console.log(days);
   }
+
+  console.log(days);
 
   // Format the age as an object
   const age = {
